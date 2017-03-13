0c1fd720a3d9ccacca94918160178b2e86460a1c
Madeleine Shang <mns_catdogmice@yahoo.ca>
2017-03-13 17:44:36 +0000

    refactor into smaller functions for easier testing / separation of concerns + improve argv processing

--- src/index.js
+++ src/index.js
@@ -1,40 +1,43 @@
-var basePx = Number(process.argv[2].replace(/\$|,/gi,"")); //remove $ and , so we can convert to number for calculation
-var people = Number(process.argv[3].replace(/people/gi,"")); //convert to number for calculation
-var pdt_category = typeof process.argv[4] === undefined ? "" :  process.argv[4].toLowerCase(); //check for null input and change to lower case for string search later
-
-
-const markup = (basePx, people, pdt_category)=>{  
+let markup = (basePx, people, pdt_category)=>{  
     var markups = [.05];        //initialzie flat markup for all products; used array for easy debugging/testing and easy operation to calculate total markup
     
+    markups.push(people*.012); //calculate the person based markup, did not split into individual help function because it's online (keeping it here makes code more readable)
+    
+    markups.push(calculate_category_markup(pdt_category)) //calcuate the category markup and append to array
+    
+    console.log(basePx*(1+markups.reduce((a,b)=>{return a+b}))); //for debugging
+    return basePx*(1+markups.reduce((a,b)=>{return a+b;})) //apply markup to base price
+}
+
+
+let calculate_category_markup = (pdt_category)=>{
     //declare state vars, use const to denote these are not expected to change, although can be updated
     const pharma=["pharmaceutical", "pharmaceuticals", "drugs", "drug", "meds", "medication"] //search array of keywords for similar terms to make code more user friendly/robust
     const food=["food", "groceries", "snack"]       //could integrate with Synonyms / NLP api down the road i.e. http://www.datamuse.com/api/
     const electronics=["electronics", "electronic"]
     
-    const category_markup = {       //keep category markups so it's easily updated (in one place) should these markups change later
+    const category_markup = {       //keep category markups so it's easily updated (in one place) should these numbers change later
         pharmaceutical: 0.075,
         food: 0.13,
         electronics: 0.02
     }
-
-    markups.push(people*.012); //calculate the person based markup, did not split into individual help function because it's online (keeping it here makes code more readable)
-    
-    //decided to use a if/else statement here because categories seems mutually exlusive (one category only); if assumption is false can switch to multiple if statements instead 
+    //decided to use a if/else statement here because categories seems mutually exlusive (one category only); if assumption is not valid can switch to multiple if statements instead 
     if (pharma.indexOf(pdt_category) !== -1){
-        markups.push(category_markup.pharmaceutical)
-    }else if (food.indexOf(pdt_category)!==-1){
-        markups.push(category_markup.food)
-    }else if (electronics.indexOf(pdt_category)!==-1){
-        markups.push(category_markup.electronics)
-    }
-    
-    // console.log(basePx, markups)
-    // console.log(basePx*(1+markups.reduce((a,b)=>{return a+b;})))
-
-    return basePx*(1+markups.reduce((a,b)=>{return a+b;}))
+            return category_markup.pharmaceutical
+        }else if (food.indexOf(pdt_category)!==-1){
+            return category_markup.food
+        }else if (electronics.indexOf(pdt_category)!==-1){
+            return category_markup.electronics
+        }else{return 0}
 }
 
-// markup(1, 5, "meds")
-
-// console.log(process.argv,process.argv[2],basePx);
-markup(basePx,people,pdt_category);
+// process inputs first, then call the main function
+try{
+    const basePx = Number(process.argv[2].replace(/\$|,/gi,"")); //remove $ and , so we can convert to number for calculation
+    const people = Number(process.argv[3].replace(/people|person/gi,"")); //convert to number for calculation
+    const pdt_category =  (' ' || process.argv[4]).toLowerCase()  ;  //check for null input and change to lower case for string search later
+    markup(basePx,people,pdt_category);     //calling the wrapper/main function with cleaned/processed parameters
+}catch(e){
+    console.log("unable to proceed with, please check inputs")   //for debugging
+    return "unable to proceed, please check inputs."
+}
