b3c810929c7ae3c146e5fd3ba9fb1f3261e9defb
Madeleine Shang <mns_catdogmice@yahoo.ca>
2017-03-22 15:21:10 +0000

    make format as libray and modify tests

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-let markup = (basePx, people, pdt_category)=>{  
-    var markups = [.05];        //initialzie flat markup for all products; used array for easy debugging/testing and easy operation to calculate total markup
+const markup = (basePx, people, pdt_category)=>{  
+    
+    basePx = basePx*1.05;
+    
+    var markups = [];        //initialzie flat markup for all products; used array for easy debugging/testing and easy operation to calculate total markup
     
     markups.push(people*.012); //calculate the person based markup, did not split into individual help function because it's online (keeping it here makes code more readable)
     
     markups.push(calculate_category_markup(pdt_category)) //calcuate the category markup and append to array
     
-    console.log(basePx*(1+markups.reduce((a,b)=>{return a+b}))); //for debugging
-    return basePx*(1+markups.reduce((a,b)=>{return a+b;})) //apply markup to base price
+    // console.log(markups, basePx, Number(basePx*(1+markups.reduce((a,b)=>{return a+b})))); //for debugging
+    return Number(basePx*(1+markups.reduce((a,b)=>{return a+b}))) //apply markup to base price
 }
 
 
-let calculate_category_markup = (pdt_category)=>{
+const calculate_category_markup = (pdt_category)=>{
     //declare state vars, use const to denote these are not expected to change, although can be updated
     const pharma=["pharmaceutical", "pharmaceuticals", "drugs", "drug", "meds", "medication"] //search array of keywords for similar terms to make code more user friendly/robust
     const food=["food", "groceries", "snack"]       //could integrate with Synonyms / NLP api down the road i.e. http://www.datamuse.com/api/
@@ -32,12 +35,24 @@ let calculate_category_markup = (pdt_category)=>{
 }
 
 // process inputs first, then call the main function
-try{
-    const basePx = Number(process.argv[2].replace(/\$|,/gi,"")); //remove $ and , so we can convert to number for calculation
-    const people = Number(process.argv[3].replace(/people|person/gi,"")); //convert to number for calculation
-    const pdt_category =  (' ' || process.argv[4]).toLowerCase()  ;  //check for null input and change to lower case for string search later
-    markup(basePx,people,pdt_category);     //calling the wrapper/main function with cleaned/processed parameters
-}catch(e){
-    console.log("unable to proceed with, please check inputs")   //for debugging
-    return "unable to proceed, please check inputs."
+const validate_input = (args)=>{
+    try{
+        args=args.split(", ")
+        // console.log(40, process.argv, "\n" ,args[0],args[1],args[2])    //for debugging
+        const basePx = Number(args[0].replace(/\$|,/gi,"")); //remove $ and , so we can convert to number for calculation
+        const people = Number(args[1].replace(/people|person/gi,"")); //convert to number for calculation
+        const pdt_category =  (args[2] ?  args[2] : ' ').toLowerCase() ;  //check for null input and change to lower case for string search later
+        // console.log(45,basePx,people,pdt_category)      //for debugging
+        return markup(basePx,people,pdt_category);     //calling the wrapper/main function with cleaned/processed parameters
+    }catch(e){
+        // console.log("unable to proceed with, please check inputs", e)   //for debugging
+        return "unable to proceed, please check inputs."
+    }
+}
+
+
+module.exports = {
+   validate_input,
+   markup,
+   calculate_category_markup
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,52 @@
-console.log("testing")
+const expect = require("expect");
+const mocha = require("mocha");
+
+const markup = require('./../src/index.js');
+
+describe('validate inputs', () => {
+    it('return error message if inputs are invalid', () => {
+        const args = [];
+        const res = markup.validate_input(args);
+        expect(res).toBe("unable to proceed, please check inputs.")
+    })
+    it('handle no cateogry', () => {
+        const args = '$1, 0 people';
+        const res = markup.validate_input(args);
+        expect(res).toBeA('number')
+    })
+})
+
+
+describe('calculate_category_markup', () => {
+    it("return the right category", () => {
+        var res = [] 
+        res.push(markup.calculate_category_markup("meds"));
+        res.push(markup.calculate_category_markup("food"));
+        res.push(markup.calculate_category_markup("electronics"));
+
+        expect(res).toEqual([0.075,0.13,0.02])
+    })
+    it("return the default of 0", () => {
+        const res = markup.calculate_category_markup("foobar");
+        expect(res).toBe(0)
+    })
+})
+
+describe('end to end tests from examples', () => {
+    it("1 - $1,299.99, 3 people, food", () => {
+        var arg ='$1,299.99, 3 people, food'
+        var res = markup.validate_input(arg)
+        expect(res.toFixed(2)).toEqual(1591.58)
+    })
+    it("2 - $5,432.00, 1 person, drugs", () => {
+        var arg ='$5,432.00, 1 person, drugs'
+        var res = markup.validate_input(arg)
+        expect(res.toFixed(2)).toEqual(6199.81)
+    })
+    it("3 - $12,456.95, 4 people, books", () => {
+        var arg ='$12,456.95, 4 people, books'
+        var res = markup.validate_input(arg)
+        expect(res.toFixed(2)).toEqual(13707.63)
+    })
+
+})
